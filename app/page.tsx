3e2b56ebfdeb75350b11f61644da0c56100ef0f6
TheDXport <therealdx10@gmail.com>
2025-08-04 05:10:12 -0400

    fixed mobile view scroll

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function Page() {
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   return (
-    <div className="relative h-1000 w-full items-center justify-items-center min-h-screen overflow-x-hidden invisible-scrollbar ">
-      <div className={`transition-opacity duration-350 w-full h-full space-y-32 `}>
+    <div className="relative w-full items-center justify-items-center min-h-screen overflow-x-hidden invisible-scrollbar">
+      <div className={`transition-opacity duration-350 w-full space-y-32 `}>
         <Header />
         <Portfolio />
       </div>
